48cee1bc105e36292058e42fe408bec7d76a8c11
Fahad Dev <fahad@srv1604347.hstgr.cloud>
2026-04-23 11:32:45 +0000

    Update HTTPS force logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') !== 'local') {
+        if (env('FORCE_HTTPS', false)) {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
     }
